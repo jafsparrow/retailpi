d1e74cbe36a0ea0af0c3bdf322e61373d9d2dfb9
Jafar <jafarcees@gmail.com>
2024-09-22 15:26:52 +0400

    (update): sales quotation ui v0.1 is okay now.

--- lib/core/providers/realm_provider.dart
+++ lib/core/providers/realm_provider.dart
@@ -2,14 +2,21 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:realm/realm.dart';
 import 'package:retailpi/core/realm_models/product_related.dart';
 import 'package:retailpi/features/pricelists/models/pricelist.dart';
+import 'package:retailpi/features/products/presentation/widgets/product_list_item.dart';
 
 final realmProvider = Provider<Realm>((ref) {
   final config = Configuration.local([
     ProductTemplateRealm.schema,
     ProductVariantRealm.schema,
     CategoryRealm.schema,
-    PriceList.schema
-  ], schemaVersion: 8);
+    PriceListRealm.schema,
+    PriceListRuleRealm.schema,
+    PosCategoryRealm.schema,
+    UomRealm.schema,
+    TaxRealm.schema,
+    ChargeRealm.schema,
+    UomCategoryRealm.schema,
+  ], schemaVersion: 9);
   final realm = Realm(config);
   return realm;
 });
--- lib/features/sales/presentation/pages/quotation_page.dart
+++ lib/features/sales/presentation/pages/quotation_page.dart
@@ -1,22 +1,60 @@
+// presentation/screens/sales_quotation_screen.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:retailpi/features/products/presentation/providers/products_provider.dart';
+import 'package:retailpi/features/sales/domain/entities/sales_quotation_line.dart';
+import 'package:retailpi/features/sales/presentation/providers/sales_quotation_provider.dart';
 
 class SalesQuotationScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final products = ref.watch(productStateNotifierProvider);
-
-    if (products.isEmpty) {
-      // Display loading indicator while products are being fetched
-      return Center(
-        child: CircularProgressIndicator(),
-      );
-    }
+    final salesQuotation = ref.watch(salesQuotationProvider);
+    final salesQuotationNotifier = ref.read(salesQuotationProvider.notifier);
 
     return Scaffold(
-        appBar: AppBar(title: Text('Sales Quotation')),
-        body: Container() /* Rest of the UI */
-        );
+      appBar: AppBar(
+        title: Text('Sales Quotation'),
+      ),
+      body: Column(
+        children: [
+          ListTile(
+            title: Text('Quotation No: ${salesQuotation.quotationNumber}'),
+            subtitle: Text('Date: ${salesQuotation.quotationDate}'),
+            trailing: Text(
+                'Total: \$${salesQuotation.totalAmount.toStringAsFixed(2)}'),
+          ),
+          Divider(),
+          Expanded(
+            child: ListView.builder(
+              itemCount: salesQuotation.quotationLines.length,
+              itemBuilder: (context, index) {
+                final line = salesQuotation.quotationLines[index];
+                return ListTile(
+                  title: Text(line.productName),
+                  subtitle: Text(
+                      'Quantity: ${line.quantity}, Price: \$${line.price}'),
+                  trailing:
+                      Text('Total: \$${line.totalPrice.toStringAsFixed(2)}'),
+                );
+              },
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // Add a new line to the quotation
+              final newLine = SalesQuotationLine(
+                productId: '123',
+                productName: 'Sample Product',
+                quantity: 1.0,
+                price: 10.0,
+                discount: 0.0,
+                totalPrice: 10.0,
+              );
+              salesQuotationNotifier.addLineToQuotation(newLine);
+            },
+            child: Text('Add Line'),
+          ),
+        ],
+      ),
+    );
   }
 }
